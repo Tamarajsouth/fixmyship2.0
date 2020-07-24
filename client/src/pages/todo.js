a48e79646a6017b8d6cae47eab87fb71bf74ea75
tamara south <tjeansouth@gmail.com>
2020-07-23 21:09:06 -0400

    small tweaks to styling and populating some data better

--- client/src/pages/todo.js
+++
@@ -1,6 +0,0 @@
-// start with the like button - handleclick sends post request yargggh?
-// find the id of the post
-// 
-
-//added to user data
-// this post with this id is being assigned to whatever the user token is
